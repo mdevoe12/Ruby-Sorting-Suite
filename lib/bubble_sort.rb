2b9827938acacabb1f483bb373ffe1532db7badc
Matt DeVoe <matthew.devoe@gmail.com>
2018-11-11 18:36:37 -0700

    slight refactor

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -8,11 +8,9 @@ class BubbleSort
   end
 
   def sort
-    size = array.length - 1
-
     return array if array.length <= 1
 
-    size.times do |i|
+    (array.length - 1).times do |i|
       if array[i] > array[i + 1]
         array[i], array[i + 1] = array[i + 1], array[i]
       end
